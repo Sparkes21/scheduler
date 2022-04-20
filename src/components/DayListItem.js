358e41a3b51f14816a5b4222d7f76f254f376112
Josh Sparkes <jshsparx@gmail.com>
2022-04-20 14:24:42 -0600

    built InterviewList component which displays a list of different interviewers and changes the look conditionally if they are selected or not

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,11 +6,13 @@ export default function DayListItem(props) {
   const formatSpots = () => {
     if (props.spots === 0) {
       return "no spots remaining"
-    } else if (props.spots === 1){
+    } 
+    if (props.spots === 1){
       return "1 spot remaining"
-    } else {
-      return `${props.spots} spots remaining`
-    }
+    } 
+    
+    return `${props.spots} spots remaining`
+    
   }
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,30 +1,22 @@
 import React from "react";
 import "components/InterviewerListItem.scss";
-import { process_params } from "express/lib/router";
 import classNames from "classnames";
 
 export default function InterviewerListItem(props) {
 
- const formatName = () => {
-   if (props.selected) {
-     return props.name
-   } else {
-     return "";
-   }
-  };
+ 
   const interviewerClass = classNames("interviewers__item", {
     "interviewers__item--selected": props.selected,
-    "day-list__item--full": props.spots === 0
   })
 
   return (
-    <li onClick={() => {props.setInterviewer(props.id)}} className={interviewerClass}>
+    <li onClick={props.setInterviewer} className={interviewerClass}>
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={props.avatar}
+        alt={props.name}
       />
-      {formatName()}
+      {props.selected && props.name}
     </li>
   )
 }
