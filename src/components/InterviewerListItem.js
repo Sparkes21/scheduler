dbd30ace51c6550345a1e274dc2a5f2fda48ae32
Josh Sparkes <jshsparx@gmail.com>
2022-04-20 10:44:19 -0600

    built InterviewerListItem component that uses props, an event handler

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "components/InterviewerListItem.scss";
+import { process_params } from "express/lib/router";
+import classNames from "classnames";
+
+export default function InterviewerListItem(props) {
+
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  })
+
+  return (
+    <li onClick={() => {props.setInterviewer(props.id)}} className={interviewerClass}>
+      <img
+        className="interviewers__item-image"
+        src="https://i.imgur.com/LpaY82x.png"
+        alt="Sylvia Palmer"
+      />
+      Sylvia Palmer
+    </li>
+  )
+}
