2a78daf9267285bf941014fc32c9349ee8af7954
Josh Sparkes <jshsparx@gmail.com>
2022-04-20 16:43:54 -0600

    built header and empty component in appointment folder

--- stories/index.js
+++ stories/index.js
@@ -9,7 +9,9 @@ import DayList from "components/DayList";
 import Button from "components/Button";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
-import Appointment from "components/Appointment/index.js";
+import Appointment from "components/Appointment";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 storiesOf("Button", module)
   .addParameters({
@@ -141,5 +143,7 @@ storiesOf("Appointment", module)
   .addParameters({
     backgrounds: [{ name: "white", value: "#fff", default: true }]
   })
-  .add("Appointment", () => (<Appointment />))
-  .add("Appointment with Time", () => <Appointment time="12pm" />);
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />);
