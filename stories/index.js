9a751ee7977596643903a74cca718fa7f72cc33b
Josh Sparkes <jshsparx@gmail.com>
2022-04-20 17:49:49 -0600

    built Form component in appointment folder and added edit/ create stories

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -171,5 +172,21 @@ storiesOf("Appointment", module)
     <Error 
       message="Could not delet appointment"
       onClose={action("onClose")}
+    />
+  ))
+  .add("Edit", () => (
+    <Form 
+      student="Josh Sparkes"
+      interviewer={4}
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Create", () => (
+    <Form 
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
     />
   ))
