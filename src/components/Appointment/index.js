21e48336e6999facd93ed83b935b8313def615c3
Josh Sparkes <jshsparx@gmail.com>
2022-04-22 19:00:01 -0600

    added mode constants and implemented our hook in the Appointment components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,20 +3,42 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode";
 
 
 
 export default function Appointment(props) {
+console.log("my props", props);
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = 'CREATE';
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+
   return (
     <Fragment>
       <article className="appointment">
         <Header time={props.time} />
-        {props.interview ? (
+
+        {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+
+        {mode === SHOW && (
           <Show
             student={props.interview.student}
             interviewer={props.interview.interviewer}
           />
-        ) : (<Empty />)}
+        )}
+        {mode === CREATE && (
+          <Form
+          // student={props.interview.student}
+          interviewers={[]}
+          onCancel= {() => back()}
+          />
+        )}
+
       </article>
     </Fragment>
   )
