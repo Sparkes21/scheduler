6e2baf7ffafabe128256a2c98751758e4b381b6b
Josh Sparkes <jshsparx@gmail.com>
2022-04-27 14:23:14 -0600

    created book an interview test

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,9 +1,10 @@
 describe("Navigation", () => {
-  it("should visit root", () => {
+  beforeEach(() => {
     cy.visit("/");
   });
+  it("should visit root", () => {
+  });
   it("should navigate to Tuesday", () => {
-    cy.visit("/");
     cy.contains("[data-testid=day]", "Tuesday").click().should("have.class", "day-list__item--selected");
   });
 
