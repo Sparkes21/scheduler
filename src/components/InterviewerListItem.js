38e07a1e961f5a7b60655d6ed002cde0e86bd496
Josh Sparkes <jshsparx@gmail.com>
2022-04-26 21:06:48 -0600

    added new tests to improve my test coverage

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -4,7 +4,7 @@ import classNames from "classnames";
 
 export default function InterviewerListItem(props) {
 
- 
+ console.log("props", props)
   const interviewerClass = classNames("interviewers__item", {
     "interviewers__item--selected": props.selected,
   })
